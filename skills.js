412506e6c1c37ac0e06d5ebc728a8ff89fd967ee
Galas <arthur.galas@capgemini.com>
2023-11-21 15:18:53 +0100

    test

--- skills.js
+++ skills.js
@@ -1,18 +1,3 @@
-function calculateDateTime(unixTime) {
-  // Convert Unix timestamp to milliseconds
-  const milliseconds = unixTime * 1000;
-
-  // Create a new Date object using the milliseconds
-  const date = new Date(milliseconds);
-
-  // Extract the date and time components
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1; // Months are zero-based, so we add 1
-  const day = date.getDate();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
-
-  // Return the formatted date and time
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+function calculateNumbers(var1, var2) {
+    console.log(var1 + var2);
 }
